83c855122c6ffbc65e5d727c2c3d6941e4ba2c97
tnbvspacesoftcom <tn.b@vspacesoft.com>
2023-12-01 12:18:49 +0530

    latest build

--- build/assets/CashMemoList-9cb34c8d.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./react-ef3a9354.js";import"./@babel-5e587562.js";const o=()=>t.createElement("h1",null,"Cash Memo List");export{o as default};
--- build/assets/Unauthorized-bab84bb4.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./react-ef3a9354.js";import"./@babel-5e587562.js";const r=()=>t.createElement("h3",null,"You are not authorized to perform the action");export{r as default};
